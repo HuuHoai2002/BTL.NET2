df344a5d047b0ca575c24ba8cf145306738d69a3
Huuhoai2002 <huuhoai0511@gmail.com>
2022-10-01 20:47:42 +0700

    update details page

--- wwwroot/js/details.js
+++ wwwroot/js/details.js
@@ -6,20 +6,15 @@ import { $ } from "./utils/selector.js";
 const movie_details = $(".app-movie-details");
 const movie_similar = $(".movie-list");
 
-const type = params.get("type");
+const type = window.location.pathname.includes("Movies") ? "movie" : "tv";
 const id = params.get("id");
 
 const details_url = `${base_url}/${type}/${id}?api_key=${api_key}&language=vi`;
 const similar_url = `${base_url}/${type}/${id}/similar?api_key=${api_key}&language=vi`;
 
 async function Main() {
-  if (type !== "movie" && type !== "tv") {
-    movie_details.innerHTML =
-      "<h1 style='padding: 12px 0; color: var(--red); font-size: 20px'>type chỉ có thể là movie hoặc tv</h1>";
-  } else {
-    await renderMovieDetails(details_url, movie_details);
-    await renderListMovie(similar_url, movie_similar);
-  }
+  await renderMovieDetails(details_url, movie_details);
+  await renderListMovie(similar_url, movie_similar);
 }
 
 Main();
